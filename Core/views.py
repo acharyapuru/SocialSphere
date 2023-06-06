50e637a71619fd2ed48b9ef6425f20bf5d41fd49
acharyapuru <acharyapuru05@gmail.com>
2023-06-06 21:42:24 +0545

    setting work done

--- Core/views.py
+++ Core/views.py
@@ -8,7 +8,8 @@ from .models import Profile
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user=Profile.objects.get(user=request.user)
+    return render(request,'index.html',{'user':user})
 
 def signup(request):
     if request.method=='POST':
@@ -25,11 +26,14 @@ def signup(request):
                 user=User.objects.create_user(username=username, email= email , password=password)
                 user.save()
 
+                user_login=authenticate(username=username,password=password)
+                login(request,user_login)
+
                 user_model = User.objects.get(username=username)
                 new_profile=Profile.objects.create(user=user_model,id_user=user_model.id,)
                 new_profile.save()
                 messages.success(request,'Account Created Successfully!!!')
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.error(request,'Password Not Matching')
             return redirect('signup')
@@ -56,3 +60,31 @@ def signout(request):
     logout(request)
     messages.success(request,'Logged out successfully!!!')
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile=Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request,'setting.html',{'user_profile':user_profile})
